7c9835205dcf34a81428f2b8ac81c2f710365741
Butikov Andrey <butikov.andrey@optimax.dev>
2023-09-16 15:47:57 +0200

    added react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import {Counter} from "./Components/Counter";
 import './index.scss';
+import {Link, Route, Routes} from "react-router-dom";
+import {MainPage} from "./Pages/MainPage/MainPage";
+import {AboutPage} from "./Pages/AboutPage/AboutPage";
 
 export const App = () => {
     return (
-        <div>
+        <div className={'app'}>
+            <Link to={'/'}>Main Page</Link>
+            <Link to={'/about'}>About Page</Link>
+            <Routes>
+                <Route path={'/'} element={<MainPage/>}/>
+                <Route path={'/about'} element={<AboutPage/>}/>
+            </Routes>
             <Counter/>
         </div>
     )
