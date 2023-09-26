e4d69148523323871cae958d2cf3a1f3cd84f4e4
Butikov Andrey <butikov.andrey@optimax.dev>
2023-09-26 17:32:35 +0200

    2-6 added webpack plugins

--- src/widgets/SideBar/ui/SideBar.tsx
+++ src/widgets/SideBar/ui/SideBar.tsx
@@ -2,7 +2,6 @@ import {classNames} from "shared/lib/classNames/classNames";
 import styles from './SideBar.module.scss';
 import React, {useState} from "react";
 import {ThemeSwitcher} from "widgets/ThemeSwitcher";
-import {useTranslation} from "react-i18next";
 import {Button} from "shared/ui/Button/Button";
 import {LangSwitcher} from "shared/ui/LangSwitcher/LangSwithcer";
 
@@ -15,15 +14,9 @@ export const SideBar = ({className}: SideBarProps) => {
 
     const [collapsed, setCollapsed] = useState(false);
 
-    const {t, i18n} = useTranslation();
-
     const onToggleCollapse = () => {
         setCollapsed(!collapsed);
     };
-
-    const onTranslate = () => {
-        i18n.changeLanguage(i18n.language === 'en' ? 'ru' : 'en');
-    }
 
     return (
         <div className={classNames(styles.SideBar, [className], {[styles.collapsed]: collapsed})}>
