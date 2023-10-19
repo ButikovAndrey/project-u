939294592b9d112aada2cf7c9e925d3a87516110
Butikov Andrey <butikov.andrey@optimax.dev>
2023-10-19 17:56:26 +0200

    2-11 added not found page and loader

--- src/app/provider/router/ui/AppRouter.tsx
+++ src/app/provider/router/ui/AppRouter.tsx
@@ -1,24 +1,27 @@
-import React, {Suspense} from 'react';
-import {Route, Routes} from "react-router-dom";
-import {routeConfig} from "shared/config/routeConfig/routeConfig";
+import React, { Suspense } from 'react';
+import { Route, Routes } from "react-router-dom";
+import { routeConfig } from "shared/config/routeConfig/routeConfig";
+import { PageLoader } from "widgets/PageLoader";
 import styles from "./AppRouter.module.scss";
 
 const AppRouter = () => {
     return (
-        <Suspense fallback={'loading...'}>
             <Routes>
-                {Object.values(routeConfig).map(({path, element}) =>
-                    (<Route
+                {Object.values(routeConfig).map(({ path, element }) =>
+                    (
+                      <Route
                         key={path}
                         path={path}
                         element={(
+                          <Suspense fallback={<PageLoader />}>
                             <div className={styles.AppRouter}>
-                                {element}
+                              {element}
                             </div>
-                        )} />
+                          </Suspense>
+                        )}
+                      />
                     ))}
             </Routes>
-        </Suspense>
     );
 };
 
---
+++ src/widgets/PageLoader/ui/PageLoader.tsx
@@ -0,0 +1,16 @@
+import classNames from "classnames";
+import { Loader } from "shared/ui/Loader/Loader";
+import styles from './PageLoader.module.scss';
+
+interface PageLoaderProps {
+    className?: string;
+
+}
+
+export const PageLoader = ({ className }: PageLoaderProps) => {
+    return (
+      <div className={classNames(styles.PageLoader, [className])}>
+        <Loader />
+      </div>
+    );
+};
